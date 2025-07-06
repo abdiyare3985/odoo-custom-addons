dcd2426c255c439ed81a75fcd8cb84ffd31e89e2
abdiyare3985 <abdiyare3985@gmail.com>
2025-07-06 13:51:48 +0300

    Made small changes

--- utility/models/res_partner_inherit.py
+++ utility/models/res_partner_inherit.py
@@ -11,12 +11,12 @@ class ResPartnerInherit(models.Model):
     #     store=True,
     #     help='The billing meter associated with this customer'
     # )
-    meter_ids = fields.One2many(
-        'billing.meter', 
-        'customer_id', 
-        string="Meters",
-        readonly=True
-    ) 
+    # meter_ids = fields.One2many(
+    #     'billing.meter', 
+    #     'customer_id', 
+    #     string="Meters",
+    #     readonly=True
+    # ) 
     meter_id = fields.Many2one(
         'billing.meter',
         string='Meter ID',
@@ -47,22 +47,23 @@ class ResPartnerInherit(models.Model):
     # # Related fields from billing.meter
     meter_serial_number = fields.Char(
         string="Meter Serial Number",
-        compute='_compute_meter_fields',
+        #compute='_compute_meter_fields',
+        related="meter_id.serial_id.serial_number",
         readonly=True
     )
     meter_zone = fields.Char(
         #comodel_name="zone",
         string="Meter Zone",
-        compute='_compute_meter_fields',
-       # related="billing_meter_id.zone_id.name",
+        #compute='_compute_meter_fields',
+        related="meter_id.zone_id.name",
         readonly=True
     )
-    meter_status = fields.Char(
-        string="Meter Status",
-        #related="billing_meter_id.status",
-        compute='_compute_meter_fields',
-        readonly=True
-    )
+    # meter_status = fields.Char(
+    #     string="Meter Status",
+    #     related="meter_id.status",
+    #     #compute='_compute_meter_fields',
+    #     readonly=True
+    # )
     discount_name = fields.Char(
         string="Discount Name",
         related="meter_id.discount_id.name",
@@ -105,29 +106,29 @@ class ResPartnerInherit(models.Model):
     #         # Add your logic here when the Billing tab is selected
     #         print("Billing tab is opened")
 
-    @api.depends('meter_ids')
-    def _compute_meter_fields(self):
-        print("Compute Meter STATUS")
-        for partner in self:
-            # if partner.meter_ids and int(partner.billing_account) > 0:
-            if partner.meter_ids:
-                # partner.meter_id=partner.meter_ids[0].id
-                # partner.meter_name=partner.meter_ids[0].name
-                # partner.meter_tariff_id = partner.meter_ids[0].tariff_id
-                partner.meter_zone = partner.meter_ids[0].zone_id.name
-                partner.meter_serial_number = partner.meter_ids[0].serial_id.serial_number
-                partner.meter_status=partner.meter_ids[0].status
-                print("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW")
-               # print(f"Meter of this customer is: {partner.meter_ids[0].status}")
-            else:
-                partner.meter_id=False
-                partner.meter_name = False
-                # partner.meter_tariff_id = False
-                # partner.meter_zone_id = False
-                # partner.meter_serial_id = False
-                # print("STATUS DOES NOT EXIST")
-                # #meter_status='Disconnected'
-                partner.meter_status = False
+    # @api.depends('meter_ids')
+    # def _compute_meter_fields(self):
+    #     print("Compute Meter STATUS")
+    #     for partner in self:
+    #         # if partner.meter_ids and int(partner.billing_account) > 0:
+    #         if partner.meter_ids:
+    #             # partner.meter_id=partner.meter_ids[0].id
+    #             # partner.meter_name=partner.meter_ids[0].name
+    #             # partner.meter_tariff_id = partner.meter_ids[0].tariff_id
+    #             partner.meter_zone = partner.meter_ids[0].zone_id.name
+    #             partner.meter_serial_number = partner.meter_ids[0].serial_id.serial_number
+    #             partner.meter_status=partner.meter_ids[0].status
+    #             print("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW")
+    #            # print(f"Meter of this customer is: {partner.meter_ids[0].status}")
+    #         else:
+    #             partner.meter_id=False
+    #             partner.meter_name = False
+    #             # partner.meter_tariff_id = False
+    #             # partner.meter_zone_id = False
+    #             # partner.meter_serial_id = False
+    #             # print("STATUS DOES NOT EXIST")
+    #             # #meter_status='Disconnected'
+    #             partner.meter_status = False
 
 
 
